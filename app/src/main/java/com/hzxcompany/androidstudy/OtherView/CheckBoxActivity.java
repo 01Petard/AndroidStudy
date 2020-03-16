2581d171c6f7665a858de2fce58b5d282e44b5cf
01Petard <1520394133@qq.com>
2020-03-16 20:59:20 +0800

    week03

---
+++ app/src/main/java/com/hzxcompany/androidstudy/OtherView/CheckBoxActivity.java
@@ -0,0 +1,16 @@
+package com.hzxcompany.androidstudy.OtherView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hzxcompany.androidstudy.R;
+
+public class CheckBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_box);
+    }
+}

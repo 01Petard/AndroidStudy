72a8df7da877c23e3eaecd2cf5132bfcb5a613d0
01Petard <1520394133@qq.com>
2020-04-02 22:57:03 +0800

    week07-ProgressBar和ProgressDialog

--- app/src/main/java/com/hzxcompany/androidstudy/ProgressBar/ProgressActivity.java
+++ app/src/main/java/com/hzxcompany/androidstudy/ProgressBar/ProgressActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
@@ -18,12 +19,13 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.hzxcompany.androidstudy.MainActivity;
 import com.hzxcompany.androidstudy.R;
 
 public class ProgressActivity extends AppCompatActivity {
 
-    private ProgressBar mPb,mPbCustome;
-    private Button mBtnStart,mBtnPd1,mBtnPd2,mBtnCustome;
+    private ProgressBar mPb, mPbCustome;
+    private Button mBtnStart, mBtnRestart, mBtnPd1, mBtnPd2, mBtnCustome,mBtnReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +33,34 @@ public class ProgressActivity extends AppCompatActivity {
         setContentView(R.layout.activity_progress);
         mPb = findViewById(R.id.pb2);
         mPbCustome = findViewById(R.id.pb3);
-        mPbCustome.setAnimation(AnimationUtils.loadAnimation(ProgressActivity.this,R.anim.icon_progress));
+        mPbCustome.setAnimation(AnimationUtils.loadAnimation(ProgressActivity.this, R.anim.icon_progress));
         mBtnPd1 = findViewById(R.id.btn_progress_dialog1);
         mBtnPd2 = findViewById(R.id.btn_progress_dialog2);
         mBtnCustome = findViewById(R.id.btn_progress_dialog3);
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        mBtnReturn = findViewById(R.id.btn_return);
         mBtnStart = findViewById(R.id.btn_start);
+
         mBtnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 handler.sendEmptyMessage(0);
             }
         });
+        mBtnRestart = findViewById(R.id.btn_restart);
+        mBtnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPb.setProgress(0);
+                handler.removeMessages(0);
+            }
+        });
+        //滚动加载条ProgressDialog
         mBtnPd1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ProgressDialog progressDialog = new ProgressDialog(ProgressActivity.this);
+                progressDialog.setIcon(R.drawable.arknights);
                 progressDialog.setTitle("提示");
                 progressDialog.setMessage("正在加载...");
                 progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
@@ -72,35 +86,64 @@ public class ProgressActivity extends AppCompatActivity {
 
             }
         });
+        //水平加载条ProgressDialog
         mBtnPd2.setOnClickListener(new View.OnClickListener() {
+            int flag=0;
             @Override
             public void onClick(View v) {
-                ProgressDialog progressDialog = new ProgressDialog(ProgressActivity.this);
+                final ProgressDialog progressDialog = new ProgressDialog(ProgressActivity.this);
                 progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.setIcon(R.drawable.arknights);
                 progressDialog.setTitle("提示");
                 progressDialog.setMessage("正在下载...");
                 progressDialog.setButton(DialogInterface.BUTTON_POSITIVE, "确定", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(ProgressActivity.this, "确定下载", Toast.LENGTH_SHORT).show();
+                        if(progressDialog.getProgress()==progressDialog.getMax()){
+                            Toast.makeText(ProgressActivity.this, "下载完成！", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(ProgressActivity.this, "下载中断！", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
                 progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "取消", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(ProgressActivity.this, "取消下载", Toast.LENGTH_SHORT).show();
+                        if(progressDialog.getProgress()!=progressDialog.getMax()){
+                            Toast.makeText(ProgressActivity.this, "下载中断！", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
                 progressDialog.show();
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        try {
+                            while (progressDialog.getProgress() < progressDialog.getMax()) {
+                                progressDialog.setProgress(progressDialog.getProgress() + 4);
+                                Thread.sleep(100);
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+                if(progressDialog.getProgress()==progressDialog.getMax()){
+                    progressDialog.setMessage("下载完成！");
+                    Toast.makeText(ProgressActivity.this, "加载完成！", Toast.LENGTH_SHORT).show();
+                }
+                progressDialog.show();
             }
         });
+        //自定义滚动加载条ProgressDialog
         mBtnCustome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View view = LayoutInflater.from(ProgressActivity.this).inflate(R.layout.layout_progressbar,null);
+                View view = LayoutInflater.from(ProgressActivity.this).inflate(R.layout.layout_progressbar, null);
                 ProgressBar pb = view.findViewById(R.id.pb_custome);
                 //给ProgressBar添加动画效果
-                pb.setAnimation(AnimationUtils.loadAnimation(ProgressActivity.this,R.anim.icon_progress));
+                pb.setAnimation(AnimationUtils.loadAnimation(ProgressActivity.this, R.anim.icon_progress));
                 builder.setTitle("提示")
                         .setView(view)//这步关键，如果不写，这个view是不会显示出来的
                         .setPositiveButton("确定", new DialogInterface.OnClickListener() {
@@ -116,15 +159,24 @@ public class ProgressActivity extends AppCompatActivity {
                 }).show();
             }
         });
+        mBtnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProgressActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    Handler handler = new Handler(){
+
+
+    Handler handler = new Handler() {
         @Override
         public void handleMessage(@NonNull Message msg) {
             super.handleMessage(msg);
-            if(mPb.getProgress()<100){
-                handler.postDelayed(runnable,100);
-            }else{
+            if (mPb.getProgress() < mPb.getMax()) {
+                handler.postDelayed(runnable, 100);
+            } else {
                 Toast.makeText(ProgressActivity.this, "加载完成！", Toast.LENGTH_SHORT).show();
             }
         }
@@ -133,7 +185,7 @@ public class ProgressActivity extends AppCompatActivity {
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
-            mPb.setProgress(mPb.getProgress()+1);
+            mPb.setProgress(mPb.getProgress() + 2);
             handler.sendEmptyMessage(0);
         }
     };

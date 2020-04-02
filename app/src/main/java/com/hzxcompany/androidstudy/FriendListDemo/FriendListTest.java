15334270b059e63b68ac737e0eb7bd2169d071b2
01Petard <1520394133@qq.com>
2020-04-02 08:59:03 +0800

    week07-UI复习作业

--- app/src/main/java/com/hzxcompany/androidstudy/FriendListDemo/FriendListTest.java
+++ app/src/main/java/com/hzxcompany/androidstudy/FriendListDemo/FriendListTest.java
@@ -2,6 +2,7 @@ package com.hzxcompany.androidstudy.FriendListDemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -20,6 +21,10 @@ public class FriendListTest extends AppCompatActivity {
         //实例化listview
         ListView lv = findViewById(R.id.lvfriend);
         //创建adapter对象
+        /*
+        Context家族很复杂，Context本身是一个抽象类，而ContextImpl，ContextWrapper，Activity，Service，Application等都是它的直接或间接子类
+        这里使用了Activity作为Context类型的参数，传入后向上转型可以调用父类的方法，使LayutInflate的from方法返回LayoutInflate对象
+         */
         FriendAdapter adapter = new FriendAdapter(this, friends);
         //设置adapter
         lv.setAdapter(adapter);
